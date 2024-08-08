2c6252d34279f98c506899d4fd86544b76207e44
Snake91reader <sks5558877@gmail.com>
2024-08-08 14:37:27 +0530

    WeatherWebApp

---
+++ script2.js
@@ -0,0 +1,55 @@
+const url =
+    'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+    'f00c38e0279b7bc85480c3fe775d518c';
+
+
+
+async function weatherFn(cName) {
+    const temp =
+        `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const res = await fetch(temp);
+        const data = await res.json();
+        if (res.ok) {
+            weatherShowFn(data);
+        } else {
+            alert('City not found. Please try again.');
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+function weatherShowFn(data) {
+    $('#city-name').text(data.name);
+    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $('#temperature').html(`${data.main.temp}&#176;C`); // Use HTML entity for the degree symbol
+    $('#description').text(data.weather[0].description);
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed} m/s`);
+
+    const weatherCondition = data.weather[0].main.toLowerCase();
+    
+    const weatherImageMap = {
+        clear: 'a.png',
+        clouds: 'b.png',
+        rain: 'c.png',
+        drizzle: 'd.png',
+        snow: 'e.png',
+        thunderstorm: 'f.png',
+        fog: 'g.png'
+    };
+
+    const weatherImage = weatherImageMap[weatherCondition] // || 'path/to/default-weather.png';
+    $('#weather-icon').attr('src', weatherImage); // Set background image based on weather condition
+
+
+    // Construct the icon URL from the API response
+   // const iconCode = data.weather[0].icon;
+   // $('#weather-icon').attr('src',`a.png`);
+    
+    $('#weather-info').fadeIn();
+
+   
+    
+}
